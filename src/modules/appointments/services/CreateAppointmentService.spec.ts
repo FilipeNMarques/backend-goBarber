138a3b176d1c3fe13651e37f389b57874a91542b
Filipe <filipen@rpc.com.br>
2020-06-20 13:42:39 -0300

    feat: add teste de criacao de usuarios

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test('Sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointsmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '234',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('234');
+  });
+
+  // it('should be not abel to create two appointments on the same date', () => {
+  //   expect().toBe();
+  // });
 });
