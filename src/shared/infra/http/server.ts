8d2cb16a49962961e2df4657ee3af07f549d46e8
Filipe Marques <filipen@rpc.com.br>
2020-07-26 16:46:46 -0300

    feat: Adicionando limite de requisicoes por ip

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,11 +9,12 @@ import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/container';
-
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 const app = express();
 const port = 3333;
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
